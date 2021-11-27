afb42f1e6770bf4b0214712af0a1321570c8c2e6
grillzwitu <grillzwitu@gmail.com>
2021-11-27 10:09:22 +0100

    pycodestyle corrections

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,9 +3,10 @@
 returns a list of lists of integers representing the Pascal’s triangle of n
 '''
 
+
 def pascal_triangle(n):
     ''' creates a pascals triangle '''
-    
+
     pTr = []
     row = []
     prev_row = []
@@ -14,7 +15,8 @@ def pascal_triangle(n):
         return []
 
     for nums in range(1, n+1):
-        row = [j > 0 and j < nums - 1 and nums > 2 and prev_row[j-1] + prev_row[j] or 1 for j in range(0, nums)]
+        row = [j > 0 and j < nums - 1 and nums > 2 and prev_row[j-1] +
+               prev_row[j] or 1 for j in range(0, nums)]
         prev_row = row
         pTr += [row]
 
