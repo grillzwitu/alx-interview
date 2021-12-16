262f6c594f8c4ffe72290f68d8a33f4f7c35b07c
grillzwitu <grillzwitu@gmail.com>
2021-12-16 23:56:41 +0100

    changed mode

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python3
-"""stats module
+"""
+Reads stdin line by line
+and computes metrics
 """
 from sys import stdin
 
 
-codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-size = 0
+stat_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
 
 
-def print_info():
-    """print_info method print needed info
-    Args:
-        codes (dict): code status
-        size (int): size of files
+def print_stats():
     """
-    print("File size: {}".format(size))
-    for key, val in sorted(codes.items()):
+    prints the stats for input in the correct give
+    every 10 lines or key interruption
+    """
+    print("File size: {}".format(total_size))
+    for key, val in sorted(stat_codes.items()):
         if val > 0:
             print("{}: {}".format(key, val))
 
+
 if __name__ == '__main__':
     try:
-        for i, line in enumerate(stdin, 1):
+        for line_num, line in enumerate(stdin, 1):
             try:
-                info = line.split()
-                size += int(info[-1])
-                if info[-2] in codes.keys():
-                    codes[info[-2]] += 1
+                stat = line.split()
+                total_size += int(stat[-1])
+                if stat[-2] in stat_codes.keys():
+                    stat_codes[stat[-2]] += 1
             except:
                 pass
-            if not i % 10:
-                print_info()
+            if not line_num % 10:
+                print_stats()
     except KeyboardInterrupt:
-        print_info()
+        print_stats()
         raise
-    print_info()
+    print_stats()
